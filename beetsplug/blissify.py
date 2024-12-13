360a8f8ede0e99bcc1636da4d55d7a264353c228
net-solution <highconcept@pm.me>
2024-12-13 22:14:54 +0000

    blissify: refactor methods into class. Multiprocessing

--- beetsplug/blissify.py
+++ beetsplug/blissify.py
@@ -3,6 +3,8 @@ blissify-rs.
 """
 
 import pickle
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from optparse import OptionParser
 
 import numpy as np
 from beets.library import Library
@@ -10,6 +12,7 @@ from beets.plugins import BeetsPlugin
 from beets.ui import Subcommand, UserError, decargs, input_options, input_yn
 from bliss_audio import Song
 from scipy.spatial import KDTree
+from tqdm import tqdm
 
 # reference:
 # album
@@ -23,170 +26,16 @@ from scipy.spatial import KDTree
 # track_number
 
 
-def analyse_library(lib: Library, opts, args):
-    """Analyse beets library with bliss"""
 class PassthruParser(OptionParser):
     def parse_args(self, args=None, values=None):
         return self, args
 
-    for item in lib.items():
-        song_path = item.path.decode("utf-8")
-        song = Song(song_path)
-
-        bliss_data = r"\␀".join(map(str, song.analysis))
-
-        # Check if song has already been analysed
-        # TODO: analyse anyway if `--force` is set
-        if hasattr(item, "bliss_data"):
-            print("Item already analysed, skipping...")
-            continue
-
-        item.bliss_data = bliss_data
-        item.store()
-
-        print(f"Analysed: {song.title} from {song.album} by {song.artist}")
-
-
-def select_song(results, page_size=10):
-    current_page = 0
-    total_pages = (len(results) + page_size - 1) // page_size
-
-    while True:
-        start = current_page * page_size
-        end = min(start + page_size, len(results))
-
-        print(f"\nPage {current_page+1} of {total_pages}")
-        for i, item in enumerate(results[start:end], start=start):
-            item_id = f"[ID: {item.id:_>6}]"
-            print(f"{i + 1}: {item.artist} - {item.title} {item_id:>18}")
-
-        if len(results) > 1:
-            print("\nEnter a number to choose a song, or pick an option")
-            choice = input_options(
-                "npq",
-                numrange=(start + 1, end),
-                default=start + 1,
-                prompt=f"[N]ext page, [P]revious page, [Q]uit, ({start+1}-{end}):",
-            )
-        else:
-            choice = 1
-
-        if isinstance(choice, int) and start + 1 <= choice <= end:
-            result_index = choice - 1
-
-            selection = results[result_index]
-            print(f"\nSelected: {selection.artist} - {selection.title}")
-            confirm = input_yn("Are you sure? [Y/n]:")
-
-            if confirm:
-                return selection
-
-        elif choice == "p":
-            current_page = (current_page - 1) % total_pages
-        elif choice == "n":
-            current_page = (current_page + 1) % total_pages
-        elif choice == "q":
-            break
-
-    return None
-
-
-def save_kdtree(tree, filepath):
-    with open(filepath, "wb") as f:
-        pickle.dump(tree, f)
-
-
-def get_nearest_songs(tree, query_vector, song_ids, k, threshold):
-    nearest_songs = []
-    offset = 0  # offset for already processed indices
-    max_requested = k
-
-    if threshold:  # get a few more results if using threshold
-        k = int(k * 1.5)
-
-    # but still using while loop, in case we didnt get enough extra
-    while len(nearest_songs) < max_requested:
-        distances, indices = tree.query(query_vector, k=k + offset, workers=-1)
-
-        # skip the already processed songs
-        indices = indices[offset:]
-        distances = distances[offset:]
-
-        new_songs = [
-            (song_ids[idx], distances[i])
-            for i, idx in enumerate(indices)
-            if distances[i] > threshold
-        ]
-
-        nearest_songs.extend(new_songs)
-        offset += k
-
-        # exit if we exhaust all neighbors
-        if len(indices) == 0:
-            break
-
-    return nearest_songs[:max_requested]
-
-
-def generate_playlist(lib: Library, opts, args):
-    """Generate a playlist of similar songs."""
-    if opts.compare:
-        results = list(lib.items(input("Enter a query: ")))
-        song1 = select_song(results, page_size=6)
-        results = list(lib.items(input("Enter a query: ")))
-        song2 = select_song(results, page_size=6)
-
-        if song1 is not None and song2 is not None:
-            song1_data = np.array(song1.bliss_data.split(r"\␀"), dtype=float)
-            print(song1_data)
-            song2_data = np.array(song2.bliss_data.split(r"\␀"), dtype=float)
-            distance = np.linalg.norm(song1_data - song2_data)
-
-            print(f"Distance between songs: {distance}")
-            return
-
-    query = decargs(args)
-
-    results = list(lib.items(query))
-    if not results:
-        print("No matching songs found!")
-        return
-
-    seed_song = select_song(results, page_size=6)
-    if seed_song is None:
-        print("Good bye")
-        return
-
-    music_library = lib.items()
-    try:
-        seed_analysis = seed_song.bliss_data.split(r"\␀")
-        song_analysis = [
-            np.array(s.bliss_data.split(r"\␀"), dtype=float)
-            for s in music_library
-        ]
-    except AttributeError:
-        print("ERROR: Missing bliss data, please (re)analyse the library!")
-        return None
-
-    # Store song ids in same order as analysis vectors
-    song_ids = [s.id for s in music_library]
-
-    # Build KDTree
-    tree = KDTree(song_analysis)
-
-    nearest_songs = get_nearest_songs(
-        tree, seed_analysis, song_ids, opts.count, opts.threshold
-    )
-
-    print("\nNearest songs:")
-    for song_id, distance in nearest_songs:
-        song = lib.get_item(song_id)
-        print(f"  * {song.artist:40} - {song.title:50} (dist: {distance:.3f})")
-
-    return
-
 
 class BlissifyPlugin(BeetsPlugin):
+    """Blissify plugin for Beets."""
+
+    MAX_WORKERS = None
+
     def __init__(self):
         super().__init__()
 
@@ -278,3 +127,226 @@ class BlissifyPlugin(BeetsPlugin):
         opts, sub_args = parser.parse_args(args)
         self.generate_playlist(lib, opts, sub_args)
 
+    # def _analyse_item(self, item):
+    #
+    #     song_path = item.path.decode("utf-8")
+    #     song = Song(song_path)
+    #
+    #     return r"\␀".join(map(str, song.analysis))
+    #
+    # def analyse_library_threads(self, lib: Library, opts):
+    #     """Analyse beets library with bliss"""
+    #
+    #     to_analyse = [
+    #         (item.id, item.path.decode("utf-8"))
+    #         for item in lib.items()
+    #         if opts.force
+    #         or not hasattr(item, "bliss_data")
+    #         or len(item.bliss_data.split(r"\␀")) != 20
+    #     ]
+    #     with ThreadPoolExecutor(max_workers=self.MAX_WORKERS) as executor:
+    #         results = list(tqdm(executor.map(self._analyse_item, to_analyse), total=len(to_analyse)))
+    #
+    #         for item in tqdm(library, total=len(library)):
+    #             bliss_data = executor.submit(self._get_bliss_data, item)
+    #
+    #             item.bliss_data = bliss_data.result()
+    #             item.store()
+
+    def _get_bliss_data(self, item):
+        song_path = item.path.decode("utf-8")
+        song = Song(song_path)
+
+        return r"\␀".join(map(str, song.analysis))
+
+    def _compute_bliss_data(self, args):
+        song_id, song_path = args
+        song = Song(song_path)
+        bliss_data = r"\␀".join(map(str, song.analysis))
+
+        return song_id, bliss_data
+
+    def _store_bliss_data(self, lib: Library, item_id: int, bliss_data: str):
+        item = lib.get_item(item_id)
+        item.bliss_data = bliss_data
+        item.store()
+
+    def analyse_library(self, lib: Library, opts):
+        """Analyse beets library with bliss."""
+
+        library = lib.items()
+        compute_args = [
+            (item.id, item.path.decode("utf-8"))
+            for item in library
+            if opts.force
+            or not hasattr(item, "bliss_data")
+            or len(item.bliss_data.split(r"\␀")) != 20
+        ]
+
+        # Compute bliss data
+        print("Analysing songs...")
+        with ProcessPoolExecutor() as executor:
+            results = list(
+                tqdm(
+                    executor.map(self._compute_bliss_data, compute_args),
+                    total=len(compute_args),
+                )
+            )
+
+        # Store results to database
+        print("Storing results...")
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self._store_bliss_data, lib, item_id, bliss_data
+                )
+                for item_id, bliss_data in results
+            ]
+            for future in tqdm(futures, total=len(futures)):
+                future.result()
+
+        return results
+
+    def select_song(self, results, page_size=10):
+        current_page = 0
+        total_pages = (len(results) + page_size - 1) // page_size
+
+        while True:
+            start = current_page * page_size
+            end = min(start + page_size, len(results))
+
+            print(f"\nPage {current_page+1} of {total_pages}")
+            for i, item in enumerate(results[start:end], start=start):
+                item_id = f"[ID: {item.id:_>6}]"
+                print(
+                    f"{i + 1}: {item.artist:40} - {item.title:50} {item_id:>18}"
+                )
+
+            if len(results) > 1:
+                print("\nEnter a number to choose a song, or pick an option")
+                choice = input_options(
+                    "npq",
+                    numrange=(start + 1, end),
+                    default=start + 1,
+                    prompt=f"[N]ext page, [P]revious page, [Q]uit, ({start+1}-{end}):",
+                )
+            else:
+                choice = 1
+
+            if isinstance(choice, int) and start + 1 <= choice <= end:
+                result_index = choice - 1
+
+                selection = results[result_index]
+                print(f"\nSelected: {selection.artist} - {selection.title}")
+                confirm = input_yn("Are you sure? [Y/n]:")
+
+                if confirm:
+                    return selection
+
+            elif choice == "p":
+                current_page = (current_page - 1) % total_pages
+            elif choice == "n":
+                current_page = (current_page + 1) % total_pages
+            elif choice == "q":
+                break
+
+        return None
+
+    def save_kdtree(self, tree, filepath):
+        with open(filepath, "wb") as f:
+            pickle.dump(tree, f)
+
+    def get_nearest_songs(self, tree, query_vector, song_ids, k, threshold):
+        nearest_songs = []
+        offset = 0  # offset for already processed indices
+        max_requested = k
+
+        if threshold:  # get a few more results if using threshold
+            k = int(k * 1.5)
+
+        # but still using while loop, in case we didnt get enough extra
+        while len(nearest_songs) < max_requested:
+            distances, indices = tree.query(
+                query_vector, k=k + offset, workers=-1
+            )
+
+            # skip the already processed songs
+            indices = indices[offset:]
+            distances = distances[offset:]
+
+            new_songs = [
+                (song_ids[idx], distances[i])
+                for i, idx in enumerate(indices)
+                if distances[i] > threshold
+            ]
+
+            nearest_songs.extend(new_songs)
+            offset += k
+
+            # exit if we exhaust all neighbors
+            if len(indices) == 0:
+                break
+
+        return nearest_songs[:max_requested]
+
+    def generate_playlist(self, lib: Library, opts, args):
+        """Generate a playlist of similar songs."""
+
+        if opts.compare:
+            results = list(lib.items(input("Enter a query: ")))
+            song1 = self.select_song(results, page_size=6)
+            results = list(lib.items(input("Enter a query: ")))
+            song2 = self.select_song(results, page_size=6)
+
+            if song1 is not None and song2 is not None:
+                song1_data = np.array(
+                    song1.bliss_data.split(r"\␀"), dtype=float
+                )
+                print(song1_data)
+                song2_data = np.array(
+                    song2.bliss_data.split(r"\␀"), dtype=float
+                )
+                distance = np.linalg.norm(song1_data - song2_data)
+
+                print(f"Distance between songs: {distance}")
+                return
+
+        query = decargs(args)
+
+        results = list(lib.items(query))
+        if not results:
+            print("No matching songs found!")
+            return
+
+        seed_song = self.select_song(results, page_size=6)
+        if seed_song is None:
+            print("Good bye")
+            return
+
+        music_library = lib.items()
+        try:
+            seed_analysis = seed_song.bliss_data.split(r"\␀")
+            song_analysis = [
+                np.array(s.bliss_data.split(r"\␀"), dtype=float)
+                for s in music_library
+            ]
+        except AttributeError:
+            print("ERROR: Missing bliss data, please (re)analyse the library!")
+            return None
+
+        # Store song ids in same order as analysis vectors
+        song_ids = [s.id for s in music_library]
+
+        # Build KDTree
+        tree = KDTree(song_analysis)
+
+        nearest_songs = self.get_nearest_songs(
+            tree, seed_analysis, song_ids, opts.count, opts.threshold
+        )
+
+        print("\nNearest songs:")
+        for song_id, distance in nearest_songs:
+            song = lib.get_item(song_id)
+            print(f"  * {song.artist:40} - {song.title:50} (dist: {distance})")
+
+        return
